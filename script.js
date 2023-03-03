1011ae41d0c361ba3c638786b68380de69df9785
Aniket2421 <115690076+Aniket2421@users.noreply.github.com>
2023-03-03 20:17:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your code here
+const textElement = document.getElementById('evaluatedText')
+const h3Element = document.getElementById('wordCount')
+
+function handelInput(e){
+  const sentence = e.target.value
+  const match = sentence.match(/\S+/ig).length;
+  h3Element.textContent = match;
+}
+textElement.addEventListener('input',handelInput)
